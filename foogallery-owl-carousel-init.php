f4f6390ab531520a07b32548b430bf241e0dd9fa
Brad Vincent <bradvin@gmail.com>
2014-07-16 17:55:26 +0200

    point to CDN image

--- foogallery-owl-carousel-init.php
+++ foogallery-owl-carousel-init.php
@@ -18,7 +18,7 @@ if ( !class_exists( 'Owl_Carousel_Template_FooGallery_Extension_Init' ) ) {
 				'description'=> 'An Owl Carousel template with multiple options for presentation and functionality. Full details on Owl Carousel <a href="http://www.owlcarousel.owlgraphic.com/">here</a>.',
 				'author'=> ' Matt Cromwell',
 				'author_url'=> 'http://mattcromwell.com',
-				'thumbnail'=> OwlC_URL . '/img/owl_carousel.jpg',
+				'thumbnail'=> 'https://s3.amazonaws.com/foogallery/extensions/owl_carousel.jpg',
 				'tags'=> array('template'),
 				'categories'=> array('Build Your Own'),
 				'source'=> 'generated'
